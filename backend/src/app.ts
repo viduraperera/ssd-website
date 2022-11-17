f616858b5064359cd6f7cc1b2d14b6d3d7d7c2b6
Dilain Gunasekara <adsgunasekara18@gmail.com>
2022-11-17 16:50:45 +0530

    ssl

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,9 @@ import cors from "cors";
 import connectToDB from "./utils/database";
 import router from "./routes";
 import deserializeUser from "./middleware/deserializeUser";
+import fs from "fs";
+import https from "https";
+import path from "path";
 
 const app = express();
 app.use(express.json());
@@ -14,9 +17,14 @@ app.use("/uploads", express.static("uploads"));
 app.use(router);
 
 const PORT = process.env.PORT || 5000;
-
-//starting app
-app.listen(PORT, () => {
-	console.log(`Server up and running on port ${PORT}`);
-	connectToDB();
+const httpsOptions = {
+	key: fs.readFileSync(path.resolve("src/key.pem")),
+	cert: fs.readFileSync(path.resolve("src/cert.pem")),
+};
+const server = https.createServer(httpsOptions, app).listen(PORT, () => {
+	console.log("server running at " + PORT);
 });
+// app.listen(PORT, () => {
+// 	console.log(`Server up and running on port ${PORT}`);
+// 	connectToDB();
+// });
